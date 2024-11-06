a2d176c193f675530d60540b42e5898707f40f52
iamsyahirah <syahirahmohammad89@gmail.com>
2024-11-06 12:41:54 +0800

    create navbar, hero

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,8 @@
-import localFont from "next/font/local";
+import Navbar from "@/components/Navbar";
 import "../styles/main.scss";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         {children}
+        <Navbar/>
       </body>
     </html>
   );
