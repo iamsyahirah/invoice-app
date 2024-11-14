b0b07106da2c8bdf193c7a76425083a09b2ebf32
iamsyahirah <syahirahmohammad89@gmail.com>
2024-11-14 23:45:32 +0800

    feat: steps component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Steps from "@/components/Steps";
 
 export default function Home() {
   return (
     <main>
-      <Hero/>
+      <Hero />
+      <Steps />
    </main>
   );
 }
--- components/Steps.jsx
+++ components/Steps.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Steps = () => {
-    return <div>Steps</div>;
+    return (
+        <div className="py-8 md:py-16 px-4 md:px-16 bg-slate-50 mx-auto flex items-center justify-center">
+            <div>
+                <h2 className="text-2xl md:text-4xl font-semibold mb-6">Create Your Invoice in Less that 2 Minutes</h2>
+                <div className="flex">
+                    <div className="flex items-center gap-3">
+                        <p className="border-2 rounded-full p-2 h-8 w-8 flex items-center justify-center border-slate-400">1</p>
+                        <p>Invoice Details</p>
+                    </div>
+                    <div className="flex items-center gap-3">
+                        <p className="border-2 rounded-full p-2 h-8 w-8 flex items-center justify-center border-slate-400">2</p>
+                        <p>Your Bank Details (Optional)</p>
+                    </div>
+                    <div className="flex items-center gap-3">
+                        <p className="border-2 rounded-full p-2 h-8 w-8 flex items-center justify-center border-slate-400">3</p>
+                        <p>Select Design & Colors (Download or Email Invoice)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default Steps;
